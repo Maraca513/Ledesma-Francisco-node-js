5c7e89b3d89e815624ff3ed889abeefe82dc241c
Maraca513 <franledesma2006@gmail.com>
2024-10-04 08:35:56 -0300

    Add files via upload
    ejercicio-2-node-js

---
+++ ej2.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")();
+let numero1 = parseFloat(prompt("Ingresa el primer número: "));
+let numero2 = parseFloat(prompt("Ingresa el segundo número: "));
+let suma = numero1 + numero2;
+console.log(suma);
